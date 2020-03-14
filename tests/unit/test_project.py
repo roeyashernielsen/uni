dabf0ae8046b28c068a8cc7fa5ebfef7d0996ded
roeyashernielsen <roey.asher@nielsen.com>
2020-03-14 18:03:44 -0400

    add docstring to tests udpate gitignore

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -1,9 +1,14 @@
+"""Test."""
 from unittest import TestCase
 
 
 class TestProject(TestCase):
+    """Test Project class."""
+
     def test_save(self):
+        """Test save."""
         self.assertTrue(True)
 
     def test_load(self):
+        """Test load."""
         self.assertTrue(True)
