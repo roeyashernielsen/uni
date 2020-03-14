dcd008d206aa06173d8f9ffec3d648059fff8d02
roeyashernielsen <roey.asher@nielsen.com>
2020-03-13 23:49:17 -0400

    go back to version file

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 """Setup functionality for the Uni package."""
 import os
-import sys
 from setuptools import setup, find_packages
 from importlib.machinery import SourceFileLoader
 
-if sys.version_info < (3, 7, 0):
-    sys.exit("Python 3.7.0 is the minimum required version")
-
-PROJECT_ROOT = os.path.dirname(__file__)
-
-about = {}
-with open(os.path.join(PROJECT_ROOT, "src", "uni", "__about__.py")) as file_:
-    exec(file_.read(), about)
+version = (
+    SourceFileLoader("uni.version", os.path.join("src", "uni", "version.py"))
+    .load_module()
+    .VERSION
+)
 
 setup(
     name="uni",
-    version=about["__version__"],
+    version=version,
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=["click>=7.0", "mlflow", "prefect", "prefect[viz]"],
     entry_points="""
@@ -23,5 +19,6 @@ setup(
             uni=uni.cli:cli
     """,
     zip_safe=False,
-    python_requires=">=3.6",
+    include_package_data=True,
+    python_requires=">=3.7",
 )
