44154f8094ea5521e72cab303bd4bce3c85c474e
roeyashernielsen <roey.asher@nielsen.com>
2020-03-13 23:40:44 -0400

    python >= 3.7 version move to __about__.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 """Setup functionality for the Uni package."""
 import os
+import sys
 from setuptools import setup, find_packages
 from importlib.machinery import SourceFileLoader
 
-version = (
-    SourceFileLoader("uni.version", os.path.join("./src/uni", "version.py"))
-    .load_module()
-    .VERSION
-)
+if sys.version_info < (3, 7, 0):
+    sys.exit("Python 3.7.0 is the minimum required version")
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(PROJECT_ROOT, "src", "uni", "__about__.py")) as file_:
+    exec(file_.read(), about)
 
 setup(
     name="uni",
-    version=version,
+    version=about["__version__"],
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=["click>=7.0", "mlflow", "prefect", "prefect[viz]"],
     entry_points="""
