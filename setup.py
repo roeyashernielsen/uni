a2932e46dbd63126a5859d4637d62060144461af
roeyashernielsen <roey.asher@nielsen.com>
2020-03-14 12:58:36 -0400

    revert hardcoded version

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 """Setup functionality for the Uni package."""
 import os
-from setuptools import setup, find_packages
 from importlib.machinery import SourceFileLoader
+from setuptools import setup, find_packages
 
-# version = (
-#     SourceFileLoader("uni.version", os.path.join("src", "uni", "version.py"))
-#     .load_module()
-#     .VERSION
-# )
+version = (
+    SourceFileLoader("uni.version", os.path.join("src", "uni", "version.py"))
+    .load_module()
+    .VERSION
+)
 
 setup(
     name="uni",
-    version="0.0.1",
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    version=version,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=["uni"],
     install_requires=["click>=7.0", "mlflow", "prefect", "prefect[viz]"],
     entry_points="""
             [console_scripts]
             uni=uni.cli:cli
     """,
-    zip_safe=False,
     include_package_data=True,
     python_requires=">=3.7",
 )
