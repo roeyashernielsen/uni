2e1756e545f60c28f1747cca1988d13c8e6ecaad
roeyashernielsen <roey.asher@nielsen.com>
2020-03-14 20:41:09 -0400

    bump and then build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="uni",
-    version="version='0.0.1',",
+    version="version='0.0.0',",
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=["uni"],
