23822b5999f75fe26d3878910bb1719521f04e14
roeyashernielsen <roey.asher@nielsen.com>
2020-03-15 12:04:32 -0400

    go back to version file

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 """Setup functionality for the Uni package."""
 
+import importlib.util
+import os
+
 from setuptools import find_packages, setup
+
+spec = importlib.util.spec_from_file_location(
+    "uni.version",
+    os.path.join("src", "uni", "version.py")
+)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
 
 setup(
     name="uni",
-    version="version='0.0.0',",
+    version=module.VERSION,
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=["uni"],
