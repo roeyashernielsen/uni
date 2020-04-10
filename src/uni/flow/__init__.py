f483643cdbb4418bc7d369dca4bf06eee46d2d9a
roeyashernielsen <roey.asher@nielsen.com>
2020-04-10 11:27:47 -0400

    add "mlflow_tracking_uri": task_instance.xcom_pull(key="mlflow_tracking_uri")

--- src/uni/flow/__init__.py
+++ src/uni/flow/__init__.py
@@ -11,7 +11,10 @@ def is_primitive(obj):
 
 def get_runs_params(task_instance, func_param):
     """Get return value from MLflow run object."""
-    result = {"mlflow_run_id": task_instance.xcom_pull(key="mlflow_run_id")}
+    result = {
+        "mlflow_run_id": task_instance.xcom_pull(key="mlflow_run_id"),
+        "mlflow_tracking_uri": task_instance.xcom_pull(key="mlflow_tracking_uri"),
+    }
     for func_name, param in func_param.items():
         run_id = task_instance.xcom_pull(task_ids=func_name)
         result.update({param: load_artifact(run_id, func_name)})
