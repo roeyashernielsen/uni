0d88d7c5c6fe76a53d4478c9c8a7c0550fcb8516
roeyashernielsen <roey.asher@nielsen.com>
2020-04-15 15:08:29 -0400

    path dir path only for spark tasks

--- src/uni/flow/ustep.py
+++ src/uni/flow/ustep.py
@@ -75,8 +75,10 @@ class _UStep:
                     mlflow.log_param(f"return_value", func_return)
 
                 if airflow_step:
-                    from dss_airflow_utils.workspace_utils import path_in_workspace
-                    path = path_in_workspace("") + "tmp"
+                    path = None
+                    if self.step_type.value == UStepType.Spark.value:
+                        from dss_airflow_utils.workspace_utils import path_in_workspace
+                        path = path_in_workspace("") + "tmp"
                     writer.save(obj=func_return, name=self.name, dir_path=path, mlflow_logging=True)
                     return run.info.run_id
                 else:
